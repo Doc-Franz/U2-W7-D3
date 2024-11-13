7740de52a31341d8ce08db64802304f6fb3e396d
Doc-Franz <federico.franzi94@gmail.com>
2024-11-13 16:50:18 +0100

    second-commit

--- js/script.js
+++ js/script.js
@@ -13,25 +13,37 @@ fetch("https://striveschool-api.herokuapp.com/books")
       <img src="${element["img"]}" class="card-img-top img-fluid" alt="..." />
       <div class="card-body">
         <h5 class="card-title">${element["title"]}</h5>
-        <h5 class="card-title">${element["price"]} $</h5>
+        <h5 class="card-price">${element["price"]} $</h5>
         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
         <a href="#" class="btn btn-primary">
           Go somewhere
-        </a>
-        <a href="#" class="btn btn-danger mt-4">
-          Scarta
-        </a>
+        </a>   
+        <a href="#" class="btn btn-danger mt-4"> Scarta </a>
+        <a href="#" class="btn btn-success mt-4"> Compra ora </a>  
       </div>
     </div>
   </div>`;
     });
     const deleteBtn = document.querySelectorAll(".btn.btn-danger");
+    const buyBtn = document.querySelectorAll(".btn.btn-success");
+    const ul = document.querySelector("ul");
 
     deleteBtn.forEach(
       (element) =>
         (element.onclick = () => {
           const cardContent = element.closest(".col.mb-4");
           cardContent.innerHTML = "";
+        })
+    );
+
+    buyBtn.forEach(
+      (element) =>
+        (element.onclick = () => {
+          const li = document.createElement("li");
+          const cardShopped = element.closest(".card");
+          const titleCardShopped = cardShopped.querySelector(".card-title");
+          li.innerText = titleCardShopped.innerText;
+          ul.appendChild(li);
         })
     );
   })
